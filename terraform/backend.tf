134f408338140f118ad0e6136ebdaa6c6a1118e0
humberto moura <humbertomoura@gmail.com>
2025-10-05 19:29:34 -0300

    fix tfstate

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,4 @@
-# Configuração do backend remoto para armazenar o estado do Terraform.
-# Isso é crucial para trabalho em equipe e para executar o Terraform em um ambiente de CI/CD.
-#
-# ANTES DE EXECUTAR 'terraform init':
-# 1. Crie um bucket S3 na sua conta AWS. Ex: "meu-projeto-tfstate-12345"
-# 2. Crie uma tabela DynamoDB com uma chave primária chamada "LockID" (do tipo String). Ex: "meu-projeto-tf-lock"
-# 3. Substitua os valores de "bucket" e "dynamodb_table" abaixo.
-#Teste
+
 
 terraform {
   backend "s3" {
